77e6fed3b23de91082874a898213522d353b21c0
Karina Ionkina <karina.ionkina15@gmail.com>
2019-05-10 13:14:24 -0400

    problem2 cpp

--- hw1/arrays-strings.cpp
+++ hw1/arrays-strings.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 
 //takes two strings and returns true if one string is a permutation of the other, false otherwise
 
@@ -24,9 +24,53 @@ bool isStringPermutation(std::string s1, std::string s2){
   return sum1 == sum2;
 }
 
+//returns an array of all pairs of integers from the input array whose sum equals the target
+
+//TO DO: Optomize to O(n) using a hashset?
+
+std::vector<std::pair<int, int> > pairsThatEqualSum(std::vector<int> inputArray,  int targetSum){
+  
+  //declaring vector to be returned
+  std::vector<std::pair<int, int> > ret;
+
+  //O(n^2):
+  for (int i = 0; i < inputArray.size() - 1; i++){
+    for (int j = i + 1; j < inputArray.size(); j++){
+      if (targetSum - inputArray[i] == inputArray[j]){
+	std::pair<int, int> p;
+	p.first = inputArray[i];
+	p.second = inputArray[j];
+	ret.push_back(p);
+      }
+    }
+  }
+  return ret;
+}
+
+
 int main(){
-  //test cases
+  //test cases fxn1
   std::cout << isStringPermutation("hello", "olehl") << std::endl; //true
   std::cout << isStringPermutation("hello", "world") << std::endl; //false
+
+  //test cases fxn2
+  std::vector<int> param1;
+  param1.push_back(0);
+  param1.push_back(1);
+  param1.push_back(2);
+  param1.push_back(3);
+  param1.push_back(4);
+  param1.push_back(5);
+
+  
+  std::vector<std::pair<int, int> > ans = pairsThatEqualSum( param1, 5); 
+  //(1,4), (2,3), (5,0)
+
+  for (int i=0; i < ans.size(); i++){
+    std::cout << "--------" << std::endl;
+    std::cout << "(" << ans.at(i).first << ", " << ans.at(i).second << ")" << std::endl;
+  }
+  
+  
   return 0;
 }
