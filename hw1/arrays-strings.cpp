b5d424fca5e42e5d737683ed2c2dd0a216f0016c
Karina Ionkina <karina.ionkina15@gmail.com>
2019-05-10 10:54:35 -0400

    work

---
+++ hw1/arrays-strings.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+//takes two strings and returns true if one string is a permutation of the other, false otherwise
+bool isStringPermutation(std::String String s1, std::String String s2){
+  
+  if (s1.length() != s2.length()){
+    return false;
+  }
+
+  //storing every char in s1 in a set
+  std::set<char> s1_chars;
+  for (int i=0; i < s1.length(); s++){
+    s1_chars.insert(s1[i]);
+  }
+}
