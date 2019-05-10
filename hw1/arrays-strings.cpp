ce5f4249f40657e028a41049524696763ea0bff7
Karina Ionkina <karina.ionkina15@gmail.com>
2019-05-10 11:43:46 -0400

    problem1 c++

--- hw1/arrays-strings.cpp
+++ hw1/arrays-strings.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 
 
-
 //takes two strings and returns true if one string is a permutation of the other, false otherwise
-bool isStringPermutation(std::String String s1, std::String String s2){
+
+//this method compares the sum of the ascii char summation of each string, which makes it a case-sensitive comparison? 
+
+bool isStringPermutation(std::string s1, std::string s2){
   if (s1.length() != s2.length()){
     return false;
   }
 
-  //storing every char in s1 in a set
-  std::set<char> s1_chars;
-  for (int i=0; i < s1.length(); s++){
-    s1_chars.insert(s1[i]);
+  //sum of ASCII values of s1 chars:
+  int sum1 = 0;
+  for (int i=0; i < s1.length(); i++){
+    sum1 += int(s1[i]);
   }
+
+  int sum2 = 0;
+  for (int i=0; i <s2.length(); i++){
+    sum2 += int(s2[i]);
+  }
+
+  return sum1 == sum2;
 }
 
-
 int main(){
-
-
-
-
-
+  //test cases
+  std::cout << isStringPermutation("hello", "olehl") << std::endl; //true
+  std::cout << isStringPermutation("hello", "world") << std::endl; //false
   return 0;
 }
